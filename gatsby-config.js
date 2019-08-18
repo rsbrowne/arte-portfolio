4e09294e5e9f6c532824bba33b54c2e0e4f41751
Ryan Browne <brownetown7@gmail.com>
2019-08-18 22:48:42 +0100

    Sets up some basic styles and variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,5 +7,14 @@
 module.exports = {
   plugins: [
     'gatsby-plugin-sass',
+    // 'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        google: {
+          families: ['Montserrat']
+        }
+      }
+    },
   ]
 }
