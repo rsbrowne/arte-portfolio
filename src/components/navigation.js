009095813aac76de079714b93a935fb895ed4ae7
Ryan Browne <brownetown7@gmail.com>
2019-08-19 00:44:01 +0100

    Adds structure and styling for sidebar

---
+++ src/components/navigation.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from 'gatsby';
+
+import instagram from '../images/instagram.svg'
+import twitter from '../images/twitter.svg'
+import linkedin from '../images/linkedin.svg'
+
+const Navigation = () => {
+
+  return (
+    <nav className="navigation">
+      <ul className="navigation__inner">
+        <li className="navigation__item">
+          <Link to="/about">About me</Link>
+        </li>
+        <li className="navigation__item">
+          <Link to="/cv">CV</Link>
+        </li>
+        <li className="navigation__item">
+          <div className="navigation__social">
+            <a href="#"><img src={twitter} alt="twitter" className="navigation__icon" /></a>
+            <a href="#"><img src={linkedin} alt="linkedin" className="navigation__icon" /></a>
+            <a href="#"><img src={instagram} alt="instagram" className="navigation__icon" /></a>
+          </div>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navigation
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { Link } from 'gatsby';
+
+import Navigation from './navigation';
+
+import logo from '../images/logo.svg'
 
 const Sidebar = () => {
 
   return (
     <aside className="sidebar">
-      <h1>Sidebar goes here</h1>
+      <div className="sidebar__content">
+        <div className="sidebar__logo">
+          <Link to="/">
+            <img src={logo} alt="Logo" />
+          </Link>
+        </div>
+        <div className="sidebar__title">
+          <h4>Artemisia Bellamy</h4>
+          <h5>UX Designer</h5>
+        </div>
+        <Navigation></Navigation>
+      </div>
     </aside>
   )
 }
