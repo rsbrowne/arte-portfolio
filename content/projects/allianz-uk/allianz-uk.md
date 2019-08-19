59aed373b0d1b290a460e8ae87f3166035f93ced
Ryan Browne <brownetown7@gmail.com>
2019-08-20 00:58:29 +0100

    Adds allianz

---
+++ content/projects/allianz-uk/allianz-uk.md
@@ -0,0 +1,10 @@
+---
+title: "Allianz UK"
+title_detail: "allianz-uk - So it is"
+slug: "/allianz-uk"
+category: "Work Stuff"
+color: "black"
+images: "allianz-uk"
+cover: ./allianz-cover.png
+---
+Test content for the allianz-uk project
--- src/components/project-grid.js
+++ src/components/project-grid.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, useStaticQuery } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 
 const ProjectGrid = () => {
@@ -19,7 +19,22 @@ const ProjectGrid = () => {
             }
           }
         }
-      }
+      },
+      allianz: allMarkdownRemark(filter: {frontmatter: {title: {eq: "Allianz UK"}}}) {
+        edges {
+          node {
+            frontmatter {
+              cover {
+                childImageSharp {
+                  fluid(maxWidth: 800) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+          }
+        }
+      },
     }
   `)
 
@@ -27,6 +42,9 @@ const ProjectGrid = () => {
     <div className="project-grid">
       <Link to="/" aria-label="PWRD" className="project-grid__item project-grid__item--pwrd">
         <Img fluid={projects.prwd.edges[0].node.frontmatter.cover.childImageSharp.fluid} />
+      </Link>
+      <Link to="/" aria-label="Allianz-UK" className="project-grid__item project-grid__item--allianz-uk">
+        <Img fluid={projects.allianz.edges[0].node.frontmatter.cover.childImageSharp.fluid} />
       </Link>
     </div>
   )
