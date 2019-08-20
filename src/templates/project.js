3ce08fe2152661dc4fa5e1b4debf3f41b97069ad
Ryan Browne <brownetown7@gmail.com>
2019-08-20 02:11:09 +0100

    Resolves annoying project issue

---
+++ src/templates/project.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from '../components/layout'
+
+const ProjectPage = () => {
+
+  return (
+    <Layout>
+      <h1>Projects will go here</h1>
+    </Layout>
+  )
+}
+
+export default ProjectPage
