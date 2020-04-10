3ddb83271cc549aff0bca9a81166a814e722b9a1
Ryan Browne <brownetown7@gmail.com>
2020-04-10 17:45:02 +0100

    Adds responsive header styles and layout

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,16 +1,24 @@
-import React from "react"
+import React, { Fragment } from "react"
 import { Link } from "gatsby"
+import useWindowSize from "./helpers/windowsize"
 
 import Navigation from "./navigation"
 
 const Sidebar = () => {
+  const { width } = useWindowSize()
+
   return (
     <aside className="sidebar">
       <div className="sidebar__content">
         <div className="sidebar__title">
           <Link to="/">
-            <h4>Artemisia Bellamy</h4>
-            <h5>UX Designer</h5>
+            {width > 600 && (
+              <Fragment>
+                <h4>Artemisia Bellemy</h4>
+                <h5>UX Designer</h5>
+              </Fragment>
+            )}
+            {width <= 600 && <h4>AB</h4>}
           </Link>
         </div>
         <Navigation />
