493f81a9483ad47ade0cc3b29d11f1619fe92731
Ryan Browne <brownetown7@gmail.com>
2020-04-10 18:12:12 +0100

    Adds links to project list

--- src/components/project-info.js
+++ src/components/project-info.js
@@ -3,6 +3,8 @@ import { graphql } from "gatsby"
 export const query = graphql`
   fragment ProjectInfo on MarkdownRemark {
     frontmatter {
+      title
+      slug
       cover {
         childImageSharp {
           fluid(maxWidth: 1600) {
