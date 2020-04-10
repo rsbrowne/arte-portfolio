504fdc34b328daf8f08821fd76b28bd0229c683d
Ryan Browne <brownetown7@gmail.com>
2020-04-10 17:57:02 +0100

    Cleans up pages

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+import "../styles/main.scss"
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <h1>About</h1>
+    </Layout>
+  )
+}
+
+export default AboutPage
