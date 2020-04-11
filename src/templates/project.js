3fd8e38575b33e7ab1c7fb2c97a92a3e8d243f98
Ryan Browne <brownetown7@gmail.com>
2020-04-11 19:59:05 +0100

    Adds initial layout for project template

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,12 +1,41 @@
 import React from "react"
 
-import Layout from '../components/layout'
+import Layout from "../components/layout"
 
 const ProjectPage = () => {
-
   return (
     <Layout>
-      <h1>Projects will go here</h1>
+      <div className="project">
+        <div className="project__intro">
+          <h3 className="project__subtitle">Project</h3>
+          <h1 className="project__title">Project Title</h1>
+          <div className="project__content">
+            <p>
+              Lorem Khaled Ipsum is a major key to success. Eliptical talk.
+              Surround yourself with angels, positive energy, beautiful people,
+              beautiful souls, clean heart, angel. Egg whites, turkey sausage,
+              wheat toast, water. Of course they don’t want us to eat our
+              breakfast, so we are going to enjoy our breakfast. The key to more
+              success is to have a lot of pillows. Put it this way, it took me
+              twenty five years to get these plants, twenty five years of blood
+              sweat and tears, and I’m never giving up, I’m just getting
+              started. Always remember in the jungle there’s a lot of they in
+              there, after you overcome they, you will make it to paradise.
+            </p>
+            <p>
+              Put it this way, it took me twenty five years to get these plants,
+              twenty five years of blood sweat and tears, and I’m never giving
+              up, I’m just getting started. Cloth talk. Another one. It’s on you
+              how you want to live your life. Everyone has a choice. I pick my
+              choice, squeaky clean. You see the hedges, how I got it shaped up?
+              It’s important to shape up your hedges, it’s like getting a
+              haircut, stay fresh. Eliptical talk.
+            </p>
+          </div>
+        </div>
+        <div className="project__images"></div>
+        <div className="project__footer"></div>
+      </div>
     </Layout>
   )
 }
