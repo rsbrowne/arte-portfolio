c3502ccc72a3c45c6b43ca9e76d0ed59dc54ce30
Ryan Browne <brownetown7@gmail.com>
2020-04-15 23:31:49 +0100

    Adds contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+import "../styles/main.scss"
+
+const ContactPage = () => {
+  return (
+    <Layout>
+      <h1>Contact</h1>
+    </Layout>
+  )
+}
+
+export default ContactPage
